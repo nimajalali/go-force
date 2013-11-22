d3184fe7e6ec2cec7dc823d99aa120ab05d99d86
Nima Jalali <nima@jalali.net>
2013-11-21 18:31:37 -0800

    revert base sobject. Added time parser by accident, not ready for prime time. Hoping Go 1.2 will help resolve some issues with json parsing.

--- sobjects/base.go
+++ sobjects/base.go
@@ -33,18 +33,3 @@ type BaseQuery struct {
 	TotalSize      float64 `json:"TotalSize" force:"totalSize"`
 	NextRecordsUri string  `json:"NextRecordsUrl" force:"nextRecordsUrl"`
 }
-
-type Time time.Time
-
-func (t *Time) UnmarshalJSON(data []byte) (err error) {
-	// Fractional seconds are handled implicitly by Parse.
-	*t, err = Parse(`"2006-01-02T15:04:05.000-0700"`, string(data))
-	return
-}
-
-func (t Time) MarshalJSON() ([]byte, error) {
-	if y := t.Year(); y < 0 || y >= 10000 {
-		return nil, errors.New("Time.MarshalJSON: year outside of range [0,9999]")
-	}
-	return []byte(t.Format(`"2006-01-02T15:04:05.000-0700"`)), nil
-}
