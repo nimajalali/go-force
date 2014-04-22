39066b0b3851703c58848de7ae45fc937ed6ddd0
Nima Jalali <nima@jalali.net>
2014-04-21 23:13:40 -0700

    Added ConvertFieldNames func. Which converts json field names to their corresponding force api names. Great when you want to dynamically set the elements after SELECT

--- sobjects/base.go
+++ sobjects/base.go
@@ -1,5 +1,25 @@
 package sobjects
 
+import (
+	"reflect"
+	"strings"
+)
+
+var baseFieldNameMap map[string]string
+
+func init() {
+	baseFieldNameMap = map[string]string{
+		"Id":               "Id",
+		"IsDeleted":        "IsDeleted",
+		"Name":             "Name",
+		"CreatedDate":      "CreatedDate",
+		"CreatedById":      "CreatedById",
+		"LastModifiedDate": "LastModifiedDate",
+		"LastModifiedById": "LastModifiedById",
+		"SystemModstamp":   "SystemModstamp",
+	}
+}
+
 // Base struct that contains fields that all objects, standard and custom, include.
 type BaseSObject struct {
 	Attributes       SObjectAttributes `json:"-" force:"attributes,omitempty"`
@@ -33,4 +53,66 @@ type BaseQuery struct {
 	Done           bool    `json:"Done" force:"done"`
 	TotalSize      float64 `json:"TotalSize" force:"totalSize"`
 	NextRecordsUri string  `json:"NextRecordsUrl" force:"nextRecordsUrl"`
+}
+
+// ConvertFieldNames takes in any interface that inplements SObject and a comma seperated list of json field names.
+// It converts the json field names to the force struct tag stated equivalent.
+func ConvertFieldNames(obj interface{}, jsonFields string) string {
+	if jsonFields != "" {
+		fields := strings.Split(jsonFields, ",")
+
+		length := len(fields)
+		if length > 0 {
+			mapping := fieldNameMapping(obj)
+
+			var forceFields []string
+			for _, field := range fields {
+				if forceField, ok := mapping[field]; ok {
+					forceFields = append(forceFields, forceField)
+				}
+			}
+
+			return strings.Join(forceFields, ",")
+		}
+	}
+
+	return ""
+}
+
+// Helper function used in ConvertFieldNames
+func fieldNameMapping(obj interface{}) map[string]string {
+	st := reflect.TypeOf(obj)
+	fl := st.NumField()
+
+	jsonToForce := make(map[string]string, fl)
+
+	for i := 0; i < fl; i++ {
+		sf := st.Field(i)
+		jName := strings.SplitN(sf.Tag.Get("json"), ",", 2)[0]
+		fName := strings.SplitN(sf.Tag.Get("force"), ",", 2)[0]
+
+		if jName == "-" {
+			continue
+		}
+
+		if fName == "-" {
+			continue
+		}
+
+		if jName == "" {
+			jName = sf.Name
+		}
+
+		if fName == "" {
+			fName = sf.Name
+		}
+
+		jsonToForce[jName] = fName
+	}
+
+	for k, v := range baseFieldNameMap {
+		jsonToForce[k] = v
+	}
+
+	return jsonToForce
 }
