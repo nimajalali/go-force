64644160474df5b6b5137ded940879e4c2fc5421
Nima Jalali <nima@jalali.net>
2014-04-21 23:14:25 -0700

    Added SFBool implementation. A bool represented as an int which allows for force api updates without overwriting boolean fields to false which is the default value of bools in Go.

---
+++ sobjects/bool.go
@@ -0,0 +1,42 @@
+// Used to represent empty bools. Go types are always instantiated with a default value, for bool the default value is false.
+// This makes it difficult to update an SObject without overwriting any boolean field to false.
+// This package solves the issue by representing a bool as an int and implementing the marshal/unmarshal json interface.
+// It is a drop in replacement for the bool type.
+// 1 is true
+// 0 is nil
+// -1 is false
+// Unmarshalling: false will be unmarshalled to -1, true will be unmarshalled to 1
+// If no value is set the unmarshaller will skip the field and the int will default to 0.
+// Marshalling: -1 will be marshaled to false, 1 will be marshaled to true, and
+// 0 will be marshaled to nothing (assuming the field has the omitempty json tag `json:",omitempty"`)
+package sobjects
+
+import "encoding/json"
+
+type SFBool int
+
+func (t *SFBool) MarshalJSON() ([]byte, error) {
+	if *t == 1 {
+		return json.Marshal(true)
+	} else if *t == -1 {
+		return json.Marshal(false)
+	}
+	return json.Marshal(0)
+}
+
+func (t *SFBool) UnmarshalJSON(data []byte) error {
+	b := string(data)
+	if b == "true" {
+		*t = 1
+	} else if b == "false" {
+		*t = -1
+	}
+	return nil
+}
+
+func (t *SFBool) Bool() bool {
+	if *t == 1 {
+		return true
+	}
+	return false
+}
