ade7131167617396a3ae79c4d4efc8d66cda9b57
Bill McCord <mccord.bill@gmail.com>
2014-09-18 19:54:55 +0800

    Added more fields to the user sobject. Added a profile sobject.

---
+++ sobjects/profile.go
@@ -0,0 +1,22 @@
+package sobjects
+
+type Profile struct {
+	BaseSObject
+	Description               string `force:",omitempty"`
+	IsSsoEnabled              bool   `force:",omitempty"`
+	LastReferencedDate        string `force:",omitempty"`
+	LastViewedDate            string `force:",omitempty"`
+	Name                      string `force:",omitempty"`
+	PermissionsPermissionName bool   `force:",omitempty"`
+	UserLicenseId             string `force:",omitempty"`
+	UserType                  string `force:",omitempty"`
+}
+
+func (t *Profile) ApiName() string {
+	return "Profile"
+}
+
+type ProfileQueryResponse struct {
+	BaseQuery
+	Records []Profile `json:"Records" force:"records"`
+}
--- sobjects/user.go
+++ sobjects/user.go
@@ -2,11 +2,19 @@ package sobjects
 
 type User struct {
 	BaseSObject
-	Email         string `force:",omitempty"`
-	FirstName     string `force:",omitempty"`
-	LastName      string `force:",omitempty"`
-	SmallPhotoUrl string `force:",omitempty"`
-	FullPhotoUrl  string `force:",omitempty"`
+	Alias             string `force:",omitempty"`
+	CommunityNickname string `force:",omitempty"`
+	Email             string `force:",omitempty"`
+	EmailEncodingKey  string `force:",omitempty"`
+	FirstName         string `force:",omitempty"`
+	FullPhotoUrl      string `force:",omitempty"`
+	LanguageLocaleKey string `force:",omitempty"`
+	LastName          string `force:",omitempty"`
+	LocaleSidKey      string `force:",omitempty"`
+	ProfileId         string `force:",omitempty"`
+	SmallPhotoUrl     string `force:",omitempty"`
+	TimeZoneSidKey    string `force:",omitempty"`
+	Username          string `force:",omitempty"`
 }
 
 func (t *User) ApiName() string {
