9f4c8e783be359c866ced960b47d76d0dd3b6c92
Bill McCord <mccord.bill@gmail.com>
2014-09-19 17:50:27 +0800

    Add opportunity sobject.

---
+++ sobjects/opportunity.go
@@ -0,0 +1,26 @@
+package sobjects
+
+type Opportunity struct {
+	BaseSObject
+	AccountId       string  `force:",omitempty"`
+	Amount          float64 `force:",omitempty"`
+	CloseDate       string  `force:",omitempty"`
+	CurrencyIsoCode string  `force:",omitempty"`
+	Description     string  `force:",omitempty"`
+	ExpectedRevenue string  `force:",omitempty"`
+	IsClosed        bool    `force:",omitempty"`
+	IsDeleted       bool    `force:",omitempty"`
+	IsSplit         bool    `force:",omitempty"`
+	IsWon           bool    `force:",omitempty"`
+	Name            string  `force:",omitempty"`
+	OwnerId         string  `force:",omitempty"`
+}
+
+func (t *Opportunity) ApiName() string {
+	return "Opportunity"
+}
+
+type OpportunityQueryResponse struct {
+	BaseQuery
+	Records []Opportunity `json:"Records" force:"records"`
+}
