3512eae66bc4590b06726a191b9e5dd60107e2b5
Elena Flat <eflat@league.com>
2018-08-21 13:47:34 -0400

    replace some nimajalali i missed

--- force/query_test.go
+++ force/query_test.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/nimajalali/go-force/sobjects"
+	"github.com/EverlongProject/go-force/sobjects"
 )
 
 const (
--- force/sobjects_test.go
+++ force/sobjects_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 	"time"
 
-	"github.com/nimajalali/go-force/sobjects"
+	"github.com/EverlongProject/go-force/sobjects"
 )
 
 const (
